093a420703431b3e841fe7705c79c4c2ca43dcc9
john ultra <johnultra@ymail.com>
2015-04-19 11:13:00 +0800

    second commit

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,6 +1,7 @@
 class SurveysController < ApplicationController
   before_action :set_survey, only: [:show, :edit, :update, :destroy]
 
+  # this is comment
   # GET /surveys
   # GET /surveys.json
   def index
